dd914d24a893948bb38c76428f57f7da3647e336
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-28 14:08:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,36 @@
+require 'pry'
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  index = 0
+  lowest_value = 500
+  name_hash.each do |element|
+    element
+    if element[key] < lowest_value
+      lowest_value = element[key]
+      binding.pry
+      "Hello"
+    end
+  end
+      lowest_value
+end
 
+
+def key_for_min_value(name_hash)
+
+index = 0
+lowest_value = 500
+while index name_hash.each do |element|
+  new_array.push(element)
+  end
+  while index < new_array.length
+    if new_array[key][value] < lowest_value
+      lowest_value = new_array[key][value]
+    end
+    index += 1
+  end
+  lowest_value
+end
 end
