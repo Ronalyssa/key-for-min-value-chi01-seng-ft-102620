98deea9e5364cb7d30abb8d846b8e713bca7a6eb
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-28 14:20:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
@@ -9,28 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |element|
     element
     if element[key][value] < lowest_value
-      lowest_value = element[key][value]
-      binding.pry
-      "Hello"
+      lowest_value = element[key]
     end
   end
       lowest_value
-end
-
-
-def key_for_min_value(name_hash)
-
-index = 0
-lowest_value = 500
-while index name_hash.each do |element|
-  new_array.push(element)
-  end
-  while index < new_array.length
-    if new_array[key][value] < lowest_value
-      lowest_value = new_array[key][value]
-    end
-    index += 1
-  end
-  lowest_value
-end
 end
