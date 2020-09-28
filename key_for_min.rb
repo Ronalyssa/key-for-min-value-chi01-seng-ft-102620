939fac961229c464e23a38b49d3f449cb4099ced
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-28 15:02:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |element|
-    lowest_value = element[1]
+    if lowest_value == nil
+      lowest_value = element[1]
+    end
     if element[1] < lowest_value
       lowest_key = element[0]
     end
