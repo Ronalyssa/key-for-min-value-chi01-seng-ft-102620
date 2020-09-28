e299ac94d14f46c21d1c66c479f4fc15982ff860
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-28 14:46:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,12 @@ require 'pry'
 def key_for_min_value(name_hash)
   index = 0
   lowest_value = 500
-  lowest_key = ""
+  lowest_key = "nil"
   name_hash.each do |element|
     element
     binding.pry
     if element[1] < lowest_value
       lowest_key = element[0]
-    elsif name_hash = {}
-      return nil
     end
   end
     lowest_key
